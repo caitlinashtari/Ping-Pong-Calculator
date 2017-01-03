c6a609db739ce4445137d1a69ddb8c454eabefa2
Caitlin Ashtari and Devin Ludwig <caitlin@>
2017-01-02 16:28:17 -0800

    install browser-sync.

--- gulpfile.js
+++ gulpfile.js
@@ -18,6 +18,26 @@ var lib = require('bower-files')({
     }
   }
 });
+var browserSync = require('browser-sync').create();
+
+gulp.task('serve', function(){
+  browserSync.init({
+    server: {
+      baseDir: "./",
+      index: "index.html"
+    }
+  });
+  gulp.watch(['js/*.js'], ['jsBuild']);
+  gulp.watch(['bower.json'], ['bowerBuild']);
+});
+
+gulp.task('jsBuild', ['jsBrowserify', 'jshint'], function(){
+  browserSync.reload();
+});
+
+gulp.task('bowerBuild',  ['bower'], function(){
+  browserSync.reload();
+});
 
 gulp.task('bowerCSS', function(){
   return gulp.src(lib.ext('css').files)
